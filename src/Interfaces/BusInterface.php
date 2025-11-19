0d3742c61713be2c21c8d9bcfc98a513122525e4
Carlos Batista <59052959+cbatista8a@users.noreply.github.com>
2025-11-19 21:25:16 +0100

    refactor: removes obsolete method on BusInterface

--- src/Interfaces/BusInterface.php
+++ src/Interfaces/BusInterface.php
@@ -17,8 +17,6 @@ interface BusInterface
 
     public function getHandler(string $identifier): string;
 
-    public function loadHandlersFromJsonFile(string $file): void;
-
     public function getHandlersDefinition(bool $with_aliases = false): array;
 
     public function getDtoClassFromAlias(string $id): string;
